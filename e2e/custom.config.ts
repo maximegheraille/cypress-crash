951f128ee28c569ac932de16b93868f7059ffef3
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2024-06-09 17:29:10 +0200

    Update custom.config.ts

--- e2e/custom.config.ts
+++ e2e/custom.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'cypress';
 
-import { defaultConfig } from './cypress-base.config';
-
 export default defineConfig({
-  ...defaultConfig,
   e2e: {
-    ...defaultConfig.e2e,
     specPattern: './cypress/integration/sf/components/**/*.ts',
-    baseUrl: 'https://www.google.com',
+    baseUrl: 'https://www.cypress.io/',
+    supportFile: 'cypress/support/index.ts',
   },
 });
