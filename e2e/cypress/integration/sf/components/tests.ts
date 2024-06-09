ca2ea986af1f91ae8f97bdc45ca4bea433899974
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2024-06-09 17:09:33 +0200

    Update tests.ts

--- e2e/cypress/integration/sf/components/tests.ts
+++ e2e/cypress/integration/sf/components/tests.ts
@@ -10,7 +10,11 @@ describe(`test`, () => {
 
   describe.only('check test', () => {
     it('check test', () => {
-      cy.get('.k1zIA.rSk4se').should('exist');
+      const person = {
+        name: 'Joe',
+        age: 20,
+      };
+      assert.isObject(person, 'value is object');
     })
   });
 });
